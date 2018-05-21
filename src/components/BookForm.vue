de9592a70aee480457fc51f1ac72fb32a90a81b3
Jon Friskics <jonfriskics@gmail.com>
2018-05-21 14:45:27 -0400

    module 5 task 4

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,9 @@
 <template>
-    
+    <form class="" action="#" method="post">
+        <input type="text" name="title" value="" placeholder="Book Title">
+        <input type="text" name="author" value="" placeholder="Book Author">
+        <button type="submit" name="button">Add Book</button>
+    </form>    
 </template>
 
 <script>
